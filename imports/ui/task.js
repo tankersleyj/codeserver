46b2a1afc7dba0adaca001f13d761b92d4ccf1ea
James Tankersely <jim.tank@live.com>
2016-04-14 05:45:13 -0500

    Public Private security

--- imports/ui/task.js
+++ imports/ui/task.js
@@ -1,7 +1,12 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
-
 import './task.html';
+
+Template.task.helpers({
+    isOwner() {
+        return this.owner === Meteor.userId();
+    },
+});
 
 Template.task.events({
     'click .toggle-checked'() {
@@ -11,4 +16,9 @@ Template.task.events({
     'click .delete'() {
         Meteor.call('tasks.remove', this._id);
     },
+
+    'click .toggle-private'() {
+        Meteor.call('tasks.setPrivate', this._id, !this.private);
+    },
+    
 });
